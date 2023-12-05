6c101969c8ec183397945d5f25c0f01fceb5bed1
Juan C. Maury <jcmauryr@gmail.com>
2023-12-05 17:42:17 -0400

    Paginador

--- app/Http/Controllers/RickAndMortyController.php
+++ app/Http/Controllers/RickAndMortyController.php
@@ -5,42 +5,54 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\GuzzleException;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 class RickAndMortyController extends Controller
 {
     public function getData()
-{
-    try {
-        // Configuración para desactivar la verificación SSL (para desarrollo)
-        $client = new Client([
-            'verify' => false,
-        ]);
+    {
+        try {
+            // Configuración para desactivar la verificación SSL (para desarrollo)
+            $client = new Client([
+                'verify' => false,
+            ]);
 
-        // Realizar una solicitud GET a la API de Rick and Morty
-        $response = $client->request('GET', 'https://rickandmortyapi.com/api/character');
+            // Realizar una solicitud GET a la API de Rick and Morty
+            $response = $client->request('GET', 'https://rickandmortyapi.com/api/character');
 
-        // Decodificar la respuesta JSON
-        $characters = json_decode($response->getBody(), true)['results'];
+            // Decodificar la respuesta JSON
+            $characters = json_decode($response->getBody(), true)['results'];
 
-        // Ordenar los personajes alfabéticamente por el campo 'name'
-        usort($characters, function ($a, $b) {
-            return strcmp($a['name'], $b['name']);
-        });
+            // Ordenar los personajes alfabéticamente por el campo 'name'
+            usort($characters, function ($a, $b) {
+                return strcmp($a['name'], $b['name']);
+            });
 
-        // Obtener detalles para cada personaje
-        foreach ($characters as &$character) {
-            $detailsResponse = $client->request('GET', 'https://rickandmortyapi.com/api/character/' . $character['id']);
-            $characterDetails = json_decode($detailsResponse->getBody(), true);
-            $character['details'] = $characterDetails;
+
+            // Obtener detalles para cada personaje
+            foreach ($characters as &$character) {
+                $detailsResponse = $client->request('GET', 'https://rickandmortyapi.com/api/character/' . $character['id']);
+                $characterDetails = json_decode($detailsResponse->getBody(), true);
+                $character['details'] = $characterDetails;
+            }
+
+            // Paginar los resultados
+            $perPage = 10; // El número de personajes por página
+            $currentPage = LengthAwarePaginator::resolveCurrentPage();
+            $currentItems = array_slice($characters, ($currentPage - 1) * $perPage, $perPage);
+            $characters = new LengthAwarePaginator($currentItems, count($characters), $perPage);
+
+            // Renderizar la vista welcome.blade.php con los datos ordenados y detalles paginados
+            return view('welcome', compact('characters'));
+
+
+            // Renderizar la vista welcome.blade.php con los datos ordenados y detalles
+            return view('welcome', compact('characters'));
+        } catch (GuzzleException $e) {
+            // Manejar errores de solicitud
+            return view('welcome', ['error' => 'Error al obtener datos de la API']);
         }
-
-        // Renderizar la vista welcome.blade.php con los datos ordenados y detalles
-        return view('welcome', compact('characters'));
-    } catch (GuzzleException $e) {
-        // Manejar errores de solicitud
-        return view('welcome', ['error' => 'Error al obtener datos de la API']);
     }
-}
 
 
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,22 +15,39 @@
                         <div class="card-body">
                             <h5 class="card-title">{{ $character['name'] }}</h5>
 
-                            <!-- Agrega más detalles según la estructura de la respuesta de la API -->
-                            @foreach($character['details'] as $key => $value)
-                                @if (!is_array($value))
-                                    <p class="card-text"><strong>{{ ucfirst($key) }}:</strong> {{ $value }}</p>
-                                @endif
-                            @endforeach
+                            @if(isset($character['details']))
+                                @foreach($character['details'] as $key => $value)
+                                    @if (!is_array($value))
+                                        <p class="card-text"><strong>{{ ucfirst($key) }}:</strong> {{ $value }}</p>
+                                    @endif
+                                @endforeach
+                            @else
+                                <p class="card-text"><strong>Detalles no disponibles</strong></p>
+                            @endif
                         </div>
                     </div>
                 </div>
             @endforeach
         </div>
 
+        {{ $characters->links() }} <!-- Agrega esta línea para mostrar la paginación -->
     @else
         <div class="alert alert-info" role="alert">
-            No hay personajes disponibles.
+            Details not available.
         </div>
     @endif
 </div>
+<script>
+
+    const hideSign = document.querySelectorAll('.w-5.h-5');
+    const hideNumbers = document.querySelectorAll('.relative.z-0.inline-flex.shadow-sm.rounded-md');
+
+    hideSign.forEach(element => {
+        element.style.display = 'none';
+    });
+
+    hideNumbers.forEach(element => {
+        element.style.display = 'none';
+    });
+</script>
 @endsection
